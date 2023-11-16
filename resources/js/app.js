c70415d7f9414d95ea93d7a0742382b3d972b434
g-dias-maciel <gabrieldmpaxa@gmail.com>
2023-11-16 22:16:18 +0100

    laravel application started + coreUI

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import './bootstrap';
+import './charts';
+import './color-modes';
+import './colors';
+import './config';
+import './main';
+import './popovers';
+import './toasts';
+import './tooltips';
+import './widgets';
+
+import.meta.glob([
+    './assets/**'
+  ]);
